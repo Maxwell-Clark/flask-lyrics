192d8305f0052e84b801707edaa6b91e9c253bed
Max <maxwell6636@gmail.com>
2021-04-08 13:06:57 -0500

    first commit

---
+++ Madlibs/Madlibs.py
@@ -0,0 +1,52 @@
+# this will be a very simple madlibs project that is filled out using the terminal.
+
+# adj1 = input("Adjective: ")
+# accomplishment = input("Accomplishment: ")
+# noun1 = input("Noun: ")
+# noun2 = input("Noun: ")
+# adj2 = input("Adjective: ")
+# adj3 = input("Adjective: ")
+
+# madlib = f"I really LOVE to be {adj1}, it has helped me accomplish {accomplishment} in my life. \
+# Because of {noun1} I can reach for the {noun2}. I have a very {adj2} Wife! And she is really {adj3}!"
+
+
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+adj5 = input("Adjective: ")
+adj6 = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+plural_noun1 = input("Plural Noun: ")
+plural_noun2 = input("Plural Noun: ")
+plural_noun3 = input("Plural Noun: ")
+plural_noun4 = input("Plural Noun: ")
+female_in_room = input("Person In room: ")
+article_of_clothing = input("Piece of Clothing: ")
+city = input("Favorite City: ")
+part_of_body = input("Part of Body: ")
+part_of_body2 = input("Part of Body: ")
+letter_of_alphabet = input("Letter of the Alphabet: ")
+celebrity = input("Celebrity: ")
+place = input("Place: ")
+website = input("Favorite Website: ")
+verb = input("Verb: ")
+number= input("Number: ")
+
+
+
+madlib = f"There are many {adj1} ways to choose a/an {noun1} to read. \
+ First, you could ask for recommendations from your friends and {plural_noun1}. \
+ Just don't ask Aunt {female_in_room} - she only reads {adj2} books with {article_of_clothing}-\
+ ripping goddesses on the cover. If your friends and family are no help, try checking out the {noun2}\
+ Review in The {city} Times. If the {plural_noun2} featured there are too {adj3} \
+ for your taste, try something a little more low-{part_of_body}, like {letter_of_alphabet}:\
+ The {celebrity} Magazine, or {plural_noun3} Magazine. You could also choose a book the \
+ {adj4} - fashioned way. Head in to your local library or {place} and browse the shelves until something\
+ catches your {part_of_body2}. Or, you could save yourself with a whole lot of {adj5} trouble\
+ and log on to {website}, the {adj6} new website to {verb} for books! \
+ With all the time you'll save not having to search for {plural_noun4}, you can read {number} more books!"
+
+print(madlib)
