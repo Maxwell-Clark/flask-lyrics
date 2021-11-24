29516b0963be38a50eeaaf0b9016bba143807e10
Max <maxwell6636@gmail.com>
2021-11-23 18:22:39 -0600

    small changes to the ui and formating

---
+++ Lyrics/flask-lyrics/main.py
@@ -0,0 +1,17 @@
+import pip._vendor.requests as requests
+
+def findLyrics(songname, songartist): 
+    # song_name = input('what is the song name? ')
+    # artist = input('what is the artists name? ')
+    print('Searching for ', songname, ' by ', songartist, '...')
+    URL = f'https://api.lyrics.ovh/v1/{songartist}/{songname}'
+    r = requests.get(url = URL)
+    data = r.json()
+    print(data['lyrics'])
+    if 'error' in data:
+        return data['error']
+    else:
+        return data['lyrics']
+
+
+# findLyrics()
